c58295e708f33c08e1004d0c3e7d6cdc6fd1ec56
kristiyan <kristiyan.tefov@icloud.com>
2023-06-08 16:32:07 +0200

    model user created

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: 
+    },
+    
+);
+
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+
+//email validator
+const validateEmail = function (email) {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+// Schema to create a new User model
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter your username"],
+    trim: true,
+    max_length: 30,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter your email"],
+    validate: [validateEmail, "Please enter a valid email"],
+    max_length: 50
+  },
+  thoughts: {
+   
+},
+  friends: {
+
+  }
+});
