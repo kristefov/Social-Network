243f3735dd833e6460a5fc48371606f861c89fde
kristiyan <kristiyan.tefov@icloud.com>
2023-06-09 02:35:53 +0200

    user controllers and routes created

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,50 @@ module.exports = {
       res.status(500).json(error);
     }
   },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .populate("thoughts")
+        .populate("friends");
+      res.json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
   async createUser(req, res) {
     try {
       const user = await User.create(req.body);
       res.json(user);
     } catch (error) {
       res.status(500).json(error);
+    }
+  },
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({ _id: req.params.userId });
+      res.status(200).json(user);
+      if (!user) {
+        return res.status(404).json({ message: "User doesn't exist." });
+      }
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      
+      if (!user) {
+        return res.status(404).json({ message: "User doesn't exist." });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+      console.error(error);
     }
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,10 +2,15 @@ const router = require('express').Router();
 
 const {
 getUsers,
-createUser
+getSingleUser,
+createUser,
+deleteUser,
+updateUser
+
 } = require('../../controllers/userController')
 
-router.route('/').get(getUsers).post(createUser)
+router.route('/').get(getUsers).post(createUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
 
 module.exports = router
